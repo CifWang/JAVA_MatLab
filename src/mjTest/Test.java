d84983ab976f4978e2845e8076afc6fe1769f3f8
CifWang <wcf@nssc.ac.cn>
2018-09-12 09:50:20 +0800

    java调用Matlab小测试

--- src/mjTest/Test.java
+++ src/mjTest/Test.java
@@ -1,12 +1,29 @@
 package mjTest;
 
+import com.mathworks.toolbox.javabuilder.MWClassID;
 import com.mathworks.toolbox.javabuilder.MWException;
+import com.mathworks.toolbox.javabuilder.MWNumericArray;
+
 import java.lang.Integer;
 
 import mySum.mySumtestMatlab;
 
 public class Test {
 	public static void main(String[] args) throws MWException{
+		MWNumericArray a=null;
+		MWNumericArray b=null;
+		
+		a=new MWNumericArray(Double.valueOf(1),MWClassID.DOUBLE);
+		b=new MWNumericArray(Double.valueOf(2),MWClassID.DOUBLE);
+		
+		MWNumericArray[] in={a,b};
+		
+		mySumtestMatlab myadd=new mySumtestMatlab();
+		Object[] result=myadd.mySum(1, in);
+		System.out.println(result[0]);
+		//Object[] result=myadd.mySum(1,a,b);
+		
+		/**
 		//String a="a";
 		//String b="b";
 		//int a=1;
@@ -18,6 +35,7 @@ public class Test {
 		//object[] result_wrong=m.mySum(1,1,2);
 		Object[] result=m.mySum(1,in);
 		System.out.println(result[0]);
+		*/
 	}
 
 }
